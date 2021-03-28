64019c3a30f46e9298d52f9bb2e840942e6dc2f5
alphachen <sdlc86@gamail.com>
2021-03-28 14:56:30 +0800

    删除debug.h

--- loop/Debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DEBUG_H_
-#define _DEBUG_H_
-
-#include <stdio.h>
-
-#define LOG(format, ...) fprintf(stderr, "%s:%d:%s->i" format "\n", __FILE__, __LINE__, __func__, __VA_ARGS__)
-
-
-#endif
