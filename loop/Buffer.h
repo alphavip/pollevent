b9414f3f8a361876519febe6d9968379663ee02e
alphachen <sdlc86@gamail.com>
2021-05-17 12:12:33 +0800

    pull commit

--- loop/Buffer.h
+++ loop/Buffer.h
@@ -17,6 +17,7 @@ struct BufferChain
     char chain[_BUFFER_CHAIN_LEN] = {0};
     uint16_t read = 0;
     uint16_t write = 0;
+    uint32_t refcount = 1;
     BufferChain* nextpkt = nullptr;
 
     inline void Init()
@@ -27,6 +28,7 @@ struct BufferChain
     {
         this->read = 0;
         this->write = 0;
+        this->refcount = 1;
         this->nextpkt = nullptr;
     }
 };
@@ -36,14 +38,28 @@ class Buffer
 public:
     inline void Clear()
     {
+        //读buff
         for(BufferChain* tmp = m_head; tmp != nullptr;)
         {
             BufferChain* bc = tmp->nextpkt;
-            Cycle(tmp);
+            assert(tmp->refcount > 0);
+            if(--tmp->refcount == 0)
+                Cycle(tmp);
             tmp = bc;
         }
         m_head = nullptr;
         m_end = nullptr;
+
+        for(BufferChain* tmp = m_head; tmp != nullptr;)
+        {
+            BufferChain* bc = tmp->nextpkt;
+            assert(tmp->refcount > 0);
+            if(--tmp->refcount)
+                Cycle(tmp);
+        }
+        m_writeHead = nullptr;
+        m_writeTail = nullptr;
+
 
         len = 0;
     }
@@ -66,6 +82,7 @@ protected:
 
 protected:
     uint32_t len = 0;
+    uint32_t writelen = 0;
     BufferChain* m_head = nullptr;
     BufferChain* m_end = nullptr;
     BufferChain* m_writeHead = nullptr;
